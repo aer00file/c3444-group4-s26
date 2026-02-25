c388d5f7b2d289e3056ccce20a1783cf5560de54
Atario64 <gradyrponegalek@gmail.com>
2026-02-24 19:53:40 -0600

    Refactor MainPage to include post display functionality
    Added PostItem class for displaying posts and integrated sample data into MainPage.

--- buddyupMainPage.py
+++ buddyupMainPage.py
@@ -1,16 +1,37 @@
+from turtle import width
 from kivy.app import App
+from kivy.uix.floatlayout import FloatLayout
+from kivy.graphics import RoundedRectangle, Color, Rectangle
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.recycleview import RecycleView
 from kivy.uix.widget import Widget
-from kivy.properties import (
-    NumericProperty, ReferenceListProperty, ObjectProperty
-)
+from kivy.properties import ObjectProperty
+from kivy.properties import StringProperty
+from kivy.lang import Builder
+
+Builder.load_file('buddyupMainPage.kv')
+
+class PostItem(BoxLayout):
+    title = StringProperty("")
+    body = StringProperty("")
 
 class MainPage(Widget):
-        pass
+    #placeholder values until database is complete
+    def on_kv_post(self, base_widget):
+        posts = [
+            {"title": "Post 1", "body": "This is the first post."},
+            {"title": "Post 2", "body": "Electric Boogaloo"},
+            {"title": "Post 3", "body": "The quest for more data entries"}
+        ]
+        
+        self.ids.rv.data = posts
 
 class BuddyUpApp(App):
     def build(self):
         return MainPage()
 
+
+
 if __name__ == '__main__':
     BuddyUpApp().run()
-         
