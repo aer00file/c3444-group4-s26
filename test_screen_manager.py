e34d1b9feb54741b2f2cbfb1a9edd98e307081bd
Atario64 <gradyrponegalek@gmail.com>
2026-04-16 07:48:03 -0500

    Change ScreenManager to MyScreenManager
    Refactor ScreenManager to MyScreenManager with ListProperty. This was to create a class that could store the search_results to be transferred to another screen.

--- test_screen_manager.py
+++ test_screen_manager.py
@@ -1,7 +1,7 @@
 from kivy.app import App
+from kivy.properties import ListProperty
 from kivy.uix.screenmanager import ScreenManager
 from kivy.lang import Builder
-
 # Load Widget files
 Builder.load_file("widgets/post_widget.kv")
 Builder.load_file("widgets/login_widget.kv")
@@ -10,10 +10,13 @@ Builder.load_file("widgets/login_widget.kv")
 from screens.post_screen import PostScreen
 from screens.login_screen import LoginScreen
 
+class MyScreenManager(ScreenManager):
+    search_results = ListProperty([])
+    
 # Actual Testing Logic
 class TestApp(App):
     def build(self):
-        sm = ScreenManager()
+        sm = MyScreenManager()
 
         sm.add_widget(LoginScreen(name="login"))
         sm.add_widget(PostScreen(name="post"))
